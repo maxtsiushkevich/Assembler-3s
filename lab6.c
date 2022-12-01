294019a544c793d40849d1ae0382f33b25fcbba3
Max Tyushkevich <92567245+maxtsiushkevich@users.noreply.github.com>
2022-12-01 11:19:58 +0300

    Update lab6.c

--- lab6.c
+++ lab6.c
@@ -1,6 +1,6 @@
 // Ввести массив чисел с плавающей точкой на 10 элементов. Для каждого 
 // элемента массива вычислить: { если Xi < 0, то Хi = (Xi)^2
-// если Xi > 0, то Хi = (Xi)^3 }.
+// если Xi 
 
 #define _CRT_SECURE_NO_WARNINGS
 #include <windows.h>
@@ -11,7 +11,6 @@
 #include <locale.h>
 
 #define SIZE 10
-#define TIMES 10000000
 using namespace std;
 
 int main()
@@ -35,60 +34,57 @@ int main()
 					rewind(stdin);
 				rewind(stdin);
 			}
-			for (int num = 0; num < TIMES; num++)
+			double sizeOfArr = SIZE;
+			double step = 1;
+			double i = 0;
+			_asm
 			{
-				double sizeOfArr = SIZE;
-				double step = 1;
-				double i = 0;
-				_asm
-				{
-					finit;
-					fld sizeOfArr
-						mov esi, 0
-						fld i;
-				loop_start:
-						fcom
-						fstsw ax 
-						and ah, 01000101b
-						je loop_end
-						fld[arr + esi]
-						fcom zero
-						fstsw ax
-						and ah, 01000101b;
-						je greaterZ
-						jmp lessZ
-				greaterZ :
-						fmul[arr + esi]
-						fstsw ax
-						and al, 00001000b
-						jne overflow
-						fmul[arr + esi]
-						fstsw ax
-						and al, 00001000b
-						jne overflow
-						fstp[resultArr + esi]
-						jmp next_step;
-				lessZ:
-						fmul[arr + esi]
-						fstsw ax
-						and al, 00001000b
-						jne overflow
-						fstp[resultArr + esi]
-						jmp next_step
-				next_step :
-						fadd step
-						fst i
-						add esi, 8
-						jmp loop_start
-				overflow :
-					fwait
-				}
-				throw new overflow_error("Переполнение!");
-				_asm
-				{
+				finit;
+				fld sizeOfArr
+					mov esi, 0
+					fld i;
+			loop_start:
+					fcom
+					fstsw ax 
+					and ah, 01000101b
+					je loop_end
+					fld[arr + esi]
+					fcom zero
+					fstsw ax
+					and ah, 01000101b;
+					je greaterZ
+					jmp lessZ
+			greaterZ :
+					fmul[arr + esi]
+					fstsw ax
+					and al, 00001000b
+					jne overflow
+					fmul[arr + esi]
+					fstsw ax
+					and al, 00001000b
+					jne overflow
+					fstp[resultArr + esi]
+					jmp next_step;
+			lessZ:
+					fmul[arr + esi]
+					fstsw ax
+					and al, 00001000b
+					jne overflow
+					fstp[resultArr + esi]
+					jmp next_step
+			next_step :
+					fadd step
+					fst i
+					add esi, 8
+					jmp loop_start
+			overflow :
+				fwait
+			}
+			throw new overflow_error("Переполнение!");
+			_asm
+			{
 				loop_end:
-					fwait
-				}
+				fwait
 			}
 			printf("\n");
 			for (int i = 0; i < SIZE; i++)
